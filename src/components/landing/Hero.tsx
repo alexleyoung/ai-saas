e392ded296c558e9771d83ee51f1a3b9dc2a4c76
alx-yng <alexyoung0119@gmail.com>
2024-08-14 13:35:03 -0500

    dev: start hero

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,5 +1,9 @@
 const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section className="h-[91.5vh] border grid place-items-center">
+      <h1 className="text-white text-4xl font-semibold">Hero</h1>
+    </section>
+  );
 };
 
 export default Hero;
